dbe7455a15c89a0c6cb277c6ba09baec68e67370
twigz20 <jerry_g16@hotmail.com>
2017-03-07 16:12:27 -0500

    Added method of retrieving snippets and creating li tags dynamically

--- public/editor/scripts/editor/js/fc/bramble-menus.js
+++ public/editor/scripts/editor/js/fc/bramble-menus.js
@@ -1,286 +1,302 @@
 define(function(require) {
 
-  var $ = require("jquery");
-  var PopupMenu = require("fc/bramble-popupmenu");
-  var analytics = require("analytics");
-  var snippets = require("fc/snippets");
+    var $ = require("jquery");
+    var PopupMenu = require("fc/bramble-popupmenu");
+    var analytics = require("analytics");
+    var snippets = require("fc/snippets");
+    var snippetsData = [];
+    var fileType = "html";
 
-  function setupUserMenu() {
-    PopupMenu.create("#navbar-logged-in .dropdown-toggle", "#navbar-logged-in .dropdown-content");
-  }
-
-  function setupLocaleMenu() {
-    PopupMenu.create("#navbar-locale .dropdown-toggle", "#navbar-locale .dropdown-content");
-  }
-  
-  function setupSnippetsMenu(bramble) {
-	  // Gear Snippets menu
-    PopupMenu.createWithOffset("#editor-pane-nav-snippets", "#editor-pane-nav-snippets-menu");
-
-	var ul = document.getElementById("editor-pane-nav-snippets-ul");
-	var snippetsObject = snippets.getSnippetObj();
-	
-	for (var key in snippetsObject) {
-	   if (snippetsObject.hasOwnProperty(key)) {
-		  var obj = snippetsObject[key];
-		  for (var prop in obj) {
-			 if (obj.hasOwnProperty(prop)) {
-				var li = document.createElement("li");
-				li.appendChild(document.createTextNode(obj[prop].name));
-				li.setAttribute("title", obj[prop].title);
-				li.setAttribute("id", obj[prop].id);
-				li.setAttribute("data-snippet", obj[prop].data);				// added line
-				ul.appendChild(li);
-				
-				$("#" + obj[prop].id).click(function() {
-					var snippet = $(this).attr("data-snippet");
-					bramble.addCodeSnippet(
-						snippet
-					);
-				});
-			 }
-		  }
-	   }
-	}
-  }
-  
-  function setupOptionsMenu(bramble) {
-    // Gear Options menu
-    PopupMenu.createWithOffset("#editor-pane-nav-options", "#editor-pane-nav-options-menu");
-
-    // Font size
-    $("#editor-pane-nav-decrease-font").click(function() {
-      bramble.decreaseFontSize(function() {
-        var fontSize = bramble.getFontSize();
-        analytics.event("DecreaseFontSize", {label: "Decreased font size to " + fontSize});
-      });
-    });
-
-    $("#editor-pane-nav-increase-font").click(function() {
-      bramble.increaseFontSize(function() {
-        var fontSize = bramble.getFontSize();
-        analytics.event("IncreaseFontSize", {label: "Increased font size to " + fontSize});
-      });
-    });
-
-
-    // Word Wrap toggle
-    function setWordWrapUI(value) {
-      if(value) {
-        $("#line-wrap-toggle").addClass("switch-enabled");
-      } else {
-        $("#line-wrap-toggle").removeClass("switch-enabled");
-      }
-    }
-    function setWordWrap(value) {
-      var method = value ? "enableWordWrap" : "disableWordWrap";
-      bramble[method](function() {
-        setWordWrapUI(value);
-        analytics.event(method);
-      });
-    }
-    $("#line-wrap-toggle").click(function() {
-      // Toggle current value
-      setWordWrap(!bramble.getWordWrap());
-      return false;
-    });
-    // Set initial UI value to match editor value
-    setWordWrapUI(bramble.getWordWrap());
-    //set initial UI value to allow javascript UI
-    if(bramble.getAllowJavaScript()) {
-        $("#allow-scripts-toggle").addClass("switch-enabled");
-    } else {
-        $("#allow-scripts-toggle").removeClass("switch-enabled");
-    }
-    // Enable/Disable JavaScript in Preview
-    $("#allow-scripts-toggle").click(function() {
-      // Toggle current value
-      var $allowScriptsToggle = $("#allow-scripts-toggle");
-      var toggle = !($allowScriptsToggle.hasClass("switch-enabled"));
-
-      if(toggle) {
-        $allowScriptsToggle.addClass("switch-enabled");
-        bramble.enableJavaScript();
-        analytics.event("EnableJavaScript");
-      } else {
-        $allowScriptsToggle.removeClass("switch-enabled");
-        bramble.disableJavaScript();
-        analytics.event("DisableJavaScript");
-      }
-
-      return false;
-    });
-
-
-    // Theme Toggle
-    function lightThemeUI() {
-      var transitionSpeed = 200;
-
-      $("#sun-green").fadeIn(transitionSpeed);
-      $("#moon-white").fadeIn(transitionSpeed);
-      $("#sun-white").fadeOut(transitionSpeed);
-      $("#moon-green").fadeOut(transitionSpeed);
-
-      // Active Indicator
-      $("#theme-active").css("position", "absolute").animate({
-        left: 35
-      }, transitionSpeed);
+    function setupUserMenu() {
+        PopupMenu.create("#navbar-logged-in .dropdown-toggle", "#navbar-logged-in .dropdown-content");
     }
 
-    function darkThemeUI() {
-      var transitionSpeed = 200;
-
-      $("#moon-green").fadeIn(transitionSpeed);
-      $("#sun-white").fadeIn(transitionSpeed);
-      $("#moon-white").fadeOut(transitionSpeed);
-      $("#sun-green").fadeOut(transitionSpeed);
-
-      // Active indicator
-      $("#theme-active").css("position", "absolute").animate({
-        left: 2
-      },transitionSpeed);
+    function setupLocaleMenu() {
+        PopupMenu.create("#navbar-locale .dropdown-toggle", "#navbar-locale .dropdown-content");
     }
 
-    function setTheme(theme) {
-      if(theme === "light-theme") {
-        bramble.useLightTheme();
-        lightThemeUI();
-        analytics.event("LightTheme");
-      } else if(theme === "dark-theme") {
-        bramble.useDarkTheme();
-        darkThemeUI();
-        analytics.event("DarkTheme");
-      }
+    function setSnippetsMenuData(bramble) {
+
+        var snippetsObject = snippets.getSnippetObj();
+        snippets.getSnippetObj();
+
+        if (snippetsObject.hasOwnProperty(fileType)) {
+            var ul = document.getElementById("editor-pane-nav-snippets-ul");
+            var obj = snippetsObject[fileType];
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    var li = document.createElement("li");
+                    li.appendChild(document.createTextNode(obj[prop].name));
+                    li.setAttribute("title", obj[prop].title);
+                    li.setAttribute("id", obj[prop].id);
+                    li.setAttribute("data-snippet", obj[prop].name);
+                    ul.appendChild(li);
+                    snippetsData[obj[prop].name] = obj[prop].data;
+
+                    $("#" + obj[prop].id).click(function() {
+                        var snippet = snippetsData[$(this).attr("data-snippet")];
+                        bramble.addCodeSnippet(
+                            snippet
+                        );
+                    });
+                }
+            }
+        }
     }
-    function toggleTheme() {
-      if(bramble.getTheme() === "dark-theme") {
-        setTheme("light-theme");
-      } else {
-        setTheme("dark-theme");
-      }
+
+    function setupSnippetsMenu(bramble) {
+        // Gear Snippets menu
+        PopupMenu.createWithOffset("#editor-pane-nav-snippets", "#editor-pane-nav-snippets-menu");
+        setSnippetsMenuData(bramble);
     }
-    $("#theme-light").click(toggleTheme);
-    $("#theme-dark").click(toggleTheme);
 
-    // If the user explicitly set the light-theme last time, use that
-    // otherwise default to using the dark-theme.
-    if(bramble.getTheme() === "light-theme") {
-      setTheme("light-theme");
-    } else {
-      setTheme("dark-theme");
+    function reloadSnippetsData(bramble, filename) {
+        fileType = filename.substring(filename.lastIndexOf('.') + 1);
+        var ul = document.getElementById("editor-pane-nav-snippets-ul");
+        ul.innerHTML = "";
+        setSnippetsMenuData(bramble);
     }
-  }
 
-  function setupAddFileMenu(bramble) {
-    var $addHtml = $("#filetree-pane-nav-add-html");
-    var $addCss = $("#filetree-pane-nav-add-css");
-    var $addJs = $("#filetree-pane-nav-add-js");
-    var $addUpload = $("#filetree-pane-nav-add-upload");
-    var $addTutorial = $("#filetree-pane-nav-add-tutorial");
+    function setupOptionsMenu(bramble) {
+        // Gear Options menu
+        PopupMenu.createWithOffset("#editor-pane-nav-options", "#editor-pane-nav-options-menu");
 
-    // Add File button and popup menu
-    var menu = PopupMenu.createWithOffset("#filetree-pane-nav-add", "#filetree-pane-nav-add-menu");
-
-    function downloadFileToFilesystem(location, fileOptions, callback) {
-      callback = callback || function noop() {};
-
-      menu.close();
-
-      $.get(location).then(function(data) {
-        fileOptions.contents = data;
-
-        bramble.addNewFile(fileOptions, function(err) {
-          if (err) {
-            console.error("[Bramble] Failed to write new file", err);
-            callback(err);
-            return;
-          }
-          callback();
+        // Font size
+        $("#editor-pane-nav-decrease-font").click(function() {
+            bramble.decreaseFontSize(function() {
+                var fontSize = bramble.getFontSize();
+                analytics.event("DecreaseFontSize", { label: "Decreased font size to " + fontSize });
+            });
         });
-      }, function(err) {
-        if (err) {
-          console.error("[Bramble] Failed to download " + location, err);
-          callback(err);
-          return;
+
+        $("#editor-pane-nav-increase-font").click(function() {
+            bramble.increaseFontSize(function() {
+                var fontSize = bramble.getFontSize();
+                analytics.event("IncreaseFontSize", { label: "Increased font size to " + fontSize });
+            });
+        });
+
+
+        // Word Wrap toggle
+        function setWordWrapUI(value) {
+            if (value) {
+                $("#line-wrap-toggle").addClass("switch-enabled");
+            } else {
+                $("#line-wrap-toggle").removeClass("switch-enabled");
+            }
         }
-        callback();
-      });
+
+        function setWordWrap(value) {
+            var method = value ? "enableWordWrap" : "disableWordWrap";
+            bramble[method](function() {
+                setWordWrapUI(value);
+                analytics.event(method);
+            });
+        }
+        $("#line-wrap-toggle").click(function() {
+            // Toggle current value
+            setWordWrap(!bramble.getWordWrap());
+            return false;
+        });
+        // Set initial UI value to match editor value
+        setWordWrapUI(bramble.getWordWrap());
+        //set initial UI value to allow javascript UI
+        if (bramble.getAllowJavaScript()) {
+            $("#allow-scripts-toggle").addClass("switch-enabled");
+        } else {
+            $("#allow-scripts-toggle").removeClass("switch-enabled");
+        }
+        // Enable/Disable JavaScript in Preview
+        $("#allow-scripts-toggle").click(function() {
+            // Toggle current value
+            var $allowScriptsToggle = $("#allow-scripts-toggle");
+            var toggle = !($allowScriptsToggle.hasClass("switch-enabled"));
+
+            if (toggle) {
+                $allowScriptsToggle.addClass("switch-enabled");
+                bramble.enableJavaScript();
+                analytics.event("EnableJavaScript");
+            } else {
+                $allowScriptsToggle.removeClass("switch-enabled");
+                bramble.disableJavaScript();
+                analytics.event("DisableJavaScript");
+            }
+
+            return false;
+        });
+
+
+        // Theme Toggle
+        function lightThemeUI() {
+            var transitionSpeed = 200;
+
+            $("#sun-green").fadeIn(transitionSpeed);
+            $("#moon-white").fadeIn(transitionSpeed);
+            $("#sun-white").fadeOut(transitionSpeed);
+            $("#moon-green").fadeOut(transitionSpeed);
+
+            // Active Indicator
+            $("#theme-active").css("position", "absolute").animate({
+                left: 35
+            }, transitionSpeed);
+        }
+
+        function darkThemeUI() {
+            var transitionSpeed = 200;
+
+            $("#moon-green").fadeIn(transitionSpeed);
+            $("#sun-white").fadeIn(transitionSpeed);
+            $("#moon-white").fadeOut(transitionSpeed);
+            $("#sun-green").fadeOut(transitionSpeed);
+
+            // Active indicator
+            $("#theme-active").css("position", "absolute").animate({
+                left: 2
+            }, transitionSpeed);
+        }
+
+        function setTheme(theme) {
+            if (theme === "light-theme") {
+                bramble.useLightTheme();
+                lightThemeUI();
+                analytics.event("LightTheme");
+            } else if (theme === "dark-theme") {
+                bramble.useDarkTheme();
+                darkThemeUI();
+                analytics.event("DarkTheme");
+            }
+        }
+
+        function toggleTheme() {
+            if (bramble.getTheme() === "dark-theme") {
+                setTheme("light-theme");
+            } else {
+                setTheme("dark-theme");
+            }
+        }
+        $("#theme-light").click(toggleTheme);
+        $("#theme-dark").click(toggleTheme);
+
+        // If the user explicitly set the light-theme last time, use that
+        // otherwise default to using the dark-theme.
+        if (bramble.getTheme() === "light-theme") {
+            setTheme("light-theme");
+        } else {
+            setTheme("dark-theme");
+        }
     }
 
-    $addHtml.click(function() {
-      var options = {
-        basenamePrefix: "index",
-        ext: ".html"
-      };
-      downloadFileToFilesystem("/default-files/html.txt", options, function(err) {
-        if (err) {
-          console.log("[Brackets] Failed to insert default HTML file", err);
-        }
-        analytics.event("AddHTMLFile");
-      });
-    });
-    $addCss.click(function() {
-      var options = {
-        basenamePrefix: "style",
-        ext: ".css"
-      };
-      downloadFileToFilesystem("/default-files/css.txt", options, function(err) {
-        if (err) {
-          console.log("[Brackets] Failed to insert default CSS file", err);
-        }
-        analytics.event("AddCSSFile");
-      });
-    });
-    $addJs.click(function() {
-      var options = {
-        basenamePrefix: "script",
-        ext: ".js"
-      };
-      downloadFileToFilesystem("/default-files/js.txt", options, function(err) {
-        if (err) {
-          console.log("[Brackets] Failed to insert default JS file", err);
-        }
-        analytics.event("AddJSFile");
-      });
-    });
-    $addTutorial.click(function() {
-      downloadFileToFilesystem("/tutorial/tutorial.html", {filename: "tutorial.html"}, function(err) {
-        if (err) {
-          console.log("[Brackets] Failed to insert tutorial.html", err);
-        }
-        analytics.event('AddTutorial');
-      });
-    });
+    function setupAddFileMenu(bramble) {
+        var $addHtml = $("#filetree-pane-nav-add-html");
+        var $addCss = $("#filetree-pane-nav-add-css");
+        var $addJs = $("#filetree-pane-nav-add-js");
+        var $addUpload = $("#filetree-pane-nav-add-upload");
+        var $addTutorial = $("#filetree-pane-nav-add-tutorial");
 
-    $addUpload.click(function() {
-      menu.close();
-      bramble.showUploadFilesDialog();
-      analytics.event("ShowUploadFilesDialog");
-    });
+        // Add File button and popup menu
+        var menu = PopupMenu.createWithOffset("#filetree-pane-nav-add", "#filetree-pane-nav-add-menu");
 
-    // We hide the add tutorial button if a tutorial exists
-    if(bramble.getTutorialExists()) {
-      $addTutorial.addClass("hide");
+        function downloadFileToFilesystem(location, fileOptions, callback) {
+            callback = callback || function noop() {};
+
+            menu.close();
+
+            $.get(location).then(function(data) {
+                fileOptions.contents = data;
+
+                bramble.addNewFile(fileOptions, function(err) {
+                    if (err) {
+                        console.error("[Bramble] Failed to write new file", err);
+                        callback(err);
+                        return;
+                    }
+                    callback();
+                });
+            }, function(err) {
+                if (err) {
+                    console.error("[Bramble] Failed to download " + location, err);
+                    callback(err);
+                    return;
+                }
+                callback();
+            });
+        }
+
+        $addHtml.click(function() {
+            var options = {
+                basenamePrefix: "index",
+                ext: ".html"
+            };
+            downloadFileToFilesystem("/default-files/html.txt", options, function(err) {
+                if (err) {
+                    console.log("[Brackets] Failed to insert default HTML file", err);
+                }
+                analytics.event("AddHTMLFile");
+            });
+        });
+        $addCss.click(function() {
+            var options = {
+                basenamePrefix: "style",
+                ext: ".css"
+            };
+            downloadFileToFilesystem("/default-files/css.txt", options, function(err) {
+                if (err) {
+                    console.log("[Brackets] Failed to insert default CSS file", err);
+                }
+                analytics.event("AddCSSFile");
+            });
+        });
+        $addJs.click(function() {
+            var options = {
+                basenamePrefix: "script",
+                ext: ".js"
+            };
+            downloadFileToFilesystem("/default-files/js.txt", options, function(err) {
+                if (err) {
+                    console.log("[Brackets] Failed to insert default JS file", err);
+                }
+                analytics.event("AddJSFile");
+            });
+        });
+        $addTutorial.click(function() {
+            downloadFileToFilesystem("/tutorial/tutorial.html", { filename: "tutorial.html" }, function(err) {
+                if (err) {
+                    console.log("[Brackets] Failed to insert tutorial.html", err);
+                }
+                analytics.event('AddTutorial');
+            });
+        });
+
+        $addUpload.click(function() {
+            menu.close();
+            bramble.showUploadFilesDialog();
+            analytics.event("ShowUploadFilesDialog");
+        });
+
+        // We hide the add tutorial button if a tutorial exists
+        if (bramble.getTutorialExists()) {
+            $addTutorial.addClass("hide");
+        }
+
+        // And listen for the user adding or removing a tutorial file
+        bramble.on("tutorialAdded", function() {
+            $addTutorial.addClass("hide");
+        });
+        bramble.on("tutorialRemoved", function() {
+            $addTutorial.removeClass("hide");
+        });
     }
 
-    // And listen for the user adding or removing a tutorial file
-    bramble.on("tutorialAdded", function() {
-      $addTutorial.addClass("hide");
-    });
-    bramble.on("tutorialRemoved", function() {
-      $addTutorial.removeClass("hide");
-    });
-  }
+    function init(bramble) {
+        setupSnippetsMenu(bramble);
+        setupOptionsMenu(bramble);
+        setupAddFileMenu(bramble);
+        setupUserMenu();
+        setupLocaleMenu();
+    }
 
-  function init(bramble) {
-	setupSnippetsMenu(bramble);
-    setupOptionsMenu(bramble);
-    setupAddFileMenu(bramble);
-    setupUserMenu();
-    setupLocaleMenu();
-  }
-
-  return {
-    init: init
-  };
+    return {
+        init: init,
+        reloadSnippetsData: reloadSnippetsData
+    };
 });
